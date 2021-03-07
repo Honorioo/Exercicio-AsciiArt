0d4ff1c691aef5d21207885dbd566bcee6346dd5
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-07 15:49:53 -0300

    Exercicio-AsciiArt

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ||  ||");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(@" \\()//");
+            Console.WriteLine(@"//(__)\\");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("||    ||");
+            Console.ResetColor();
+
+            
+
+        }
+    }
+}
